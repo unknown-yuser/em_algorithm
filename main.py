25f649555b7080a88deca4df294707ac938ccab3
Yusuke Kanai <yusuke.kanai.gh@gmail.com>
2018-01-01 03:03:46 +0900

    [Mod] 1. Modify init distribution component / 2. Print multiple sample

--- main.py
+++ main.py
@@ -15,44 +15,26 @@ from matplotlib import pyplot
 
 class EMAlgorithm(object):
     def __init__(self):
-        self.cls_num = 100
-        self.pca = decomposition.PCA(n_components=30)
+        self.samples = 2000
+        self.pca_dim = 50
+        self.pca = decomposition.PCA(n_components=self.pca_dim)
 
         mnist_data = mnist.train_images().reshape(-1, 784) / 255.
-        mnist_ks = []
-        self.label_data = mnist.train_labels()
-
-        for i in range(10):
-            mnist_ks.append(mnist_data[self.label_data == i][0:self.cls_num])
-        self.image_data = np.concatenate(mnist_ks)
-
+        self.image_data = mnist_data[0:self.samples]
         self.pca.fit(self.image_data)
         self.image_data = self.pca.transform(self.image_data)
 
         # mixing coefficient
         self.mc = [0.1] * 10
-        self.init_component(self.image_data)
-
-    def init_component(self, images):
-        c = []
-        m = []
-        v = []
-        for i in range(10):
-            c.append(images[i * self.cls_num:(i + 1) * self.cls_num])
-
-        for i in range(10):
-            m.append(np.mean(c[i], axis=0))
-            vi = np.cov(c[i], rowvar=0, bias=1)
-            v.append(vi)
-
-        self.comp_mean = m
-        self.comp_var = v
+        self.comp_mean = [np.random.random(self.pca_dim) for i in range(10)]
+        self.comp_var = [np.identity(self.pca_dim, dtype='f')] * 10
 
     def e_step(self, datas):
         gen_prob = []
 
         for i in range(10):
-            gen_prob_k = self.mc[i] * multivariate_normal.pdf(datas, mean=self.comp_mean[i], cov=self.comp_var[i])
+            gen_prob_k = self.mc[i] * multivariate_normal.pdf(datas, mean=self.comp_mean[i], cov=self.comp_var[i],
+                                                              allow_singular=True)
             gen_prob.append(gen_prob_k)
 
         gen_prob = gen_prob / sum(gen_prob)
@@ -64,7 +46,7 @@ class EMAlgorithm(object):
             self.comp_mean[i] = datas.transpose().dot(brob[i]) / nk[i]
         for i in range(10):
             diff = datas - self.comp_mean[i]
-            self.comp_var[i] = diff.transpose().dot(np.multiply(diff, brob[i].reshape((10*self.cls_num, 1)))) / nk[i]
+            self.comp_var[i] = diff.transpose().dot(np.multiply(diff, brob[i].reshape((self.samples, 1)))) / nk[i]
         self.mc = nk / datas.shape[0]
 
     def log_likelihood(self, datas):
@@ -72,23 +54,27 @@ class EMAlgorithm(object):
         for data in datas:
             res_i = 0.
             for i in range(10):
-                res_i += self.mc[i] * multivariate_normal.pdf(data, mean=self.comp_mean[i], cov=self.comp_var[i])
+                res_i += self.mc[i] * multivariate_normal.pdf(data, mean=self.comp_mean[i], cov=self.comp_var[i],
+                                                              allow_singular=True)
             res += np.log(res_i)
         return res
 
-    def print_label(self, name, k):
-        output = multivariate_normal.rvs(mean=self.comp_mean[k], cov=self.comp_var[k])
-        output = 255 * self.pca.inverse_transform(output).reshape((28, 28))
+    def print_label(self, name):
+        result = []
+        for i in range(10):
+            result.append(multivariate_normal.rvs(mean=self.comp_mean[i], cov=self.comp_var[i], size=10))
+        result = self.pca.inverse_transform(np.stack(result, axis=0)).reshape(10, 10, 28, 28)
+        output = 255 * result.transpose(0, 2, 1, 3).reshape((10 * 28, 10 * 28))
         pyplot.imshow(output)
         pyplot.savefig(name)
 
     def train(self):
-        epoch = 10
+        epoch = 20
         for i in range(epoch):
             prob = self.e_step(self.image_data)
             self.m_step(self.image_data, prob)
             print(self.log_likelihood(self.image_data))
-            self.print_label("sample_{}".format(i), i)
+            self.print_label("sample_{}".format(i))
 
 
 def main():
